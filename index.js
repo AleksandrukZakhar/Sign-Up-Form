d0225471e7e49b2ad8ad2328a2ca8684af30de2e
AleksandrukZakhar <zakharaleksandrukdev@gmail.com>
2022-08-15 15:04:04 +0300

    Add validation

---
+++ index.js
@@ -0,0 +1,12 @@
+const form = document.querySelector("form");
+const pass = document.getElementById("pass");
+const confirm = document.getElementById("confirm");
+const error = document.getElementById("error");
+
+form.addEventListener("submit", (e) => {
+  if (pass.value === confirm.value) return;
+
+  e.preventDefault();
+
+  error.textContent = "Passwords do not match";
+});
